f76246201cab232bea49bdefff9d1e5d376a7759
Teifion <teifion@teifion.co.uk>
2023-04-30 16:48:00 +0100

    Removed some boilerplate files

--- lib/balance_algorithm.ex
+++ lib/balance_algorithm.ex
@@ -2,17 +2,4 @@ defmodule BalanceAlgorithm do
   @moduledoc """
   Documentation for `BalanceAlgorithm`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BalanceAlgorithm.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/balance_algorithm_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule BalanceAlgorithmTest do
-  use ExUnit.Case
-  doctest BalanceAlgorithm
-
-  test "greets the world" do
-    assert BalanceAlgorithm.hello() == :world
-  end
-end
